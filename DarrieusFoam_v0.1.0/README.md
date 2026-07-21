ddd2e413c10e0d156fa5aee26907b34e9179df39
normandajc <normandajc@laposte.net>
2026-07-21 10:29:17 +0200

    Add files via upload

---
+++ DarrieusFoam_v0.1.0/README.md
@@ -0,0 +1,3 @@
+# DarrieusFoam
+
+Bootstrap repository for a 2D H-Darrieus OpenFOAM v2506 overset framework.
---
+++ DarrieusFoam_v0.1.0/geometry/nacaGenerator.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""Placeholder NACA generator (to be completed in v0.2.0)."""
+print("DarrieusFoam NACA generator")
